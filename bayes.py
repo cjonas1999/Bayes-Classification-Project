2c33164ba59fecd25e76b34fadbe571518eb79b5
Casey Jonas <cjonas1999@gmail.com>
2020-11-01 20:29:44 -0600

    Updated constructor to account for classification in training data

--- bayes.py
+++ bayes.py
@@ -1,7 +1,7 @@
 class Bayes:
 	def __init__(self, meta_filename, training_filename):
-		self.classNames = []
-		self.classes = []
+		self.attributes = [] #the classifications are the last item in self.attributes
+		self.counts = [] #counts is accessed in this format: self.counts[column][classification][attribute]
 		
 		#Read meta file
 		meta_f = open(meta_filename, "r")
@@ -9,26 +9,44 @@ class Bayes:
 		for line in meta_f:
 			n = line.strip().split(':')
 
-			self.classNames.append(n[0])
-			self.classes.append({})
+			self.attributes.append({})
+			self.attributes[i][n[0]] = set()
 
 			for val in n[1].split(','):
-				self.classes[i][val] = 1
+				self.attributes[i][n[0]].add(val)
 			
 			i += 1
 		
 		meta_f.close()
-		
 
-		#read training file
+
+		#initialize counts
+		i = 0
+		for attr in self.attributes[:-1]:
+			self.counts.append({})
+			for classif in self.attributes[-1].values():
+				for v in classif:
+					self.counts[i][v] = {}
+
+					for c in self.attributes[i].values():
+						for attr in c:
+							self.counts[i][v][attr] = 0
+
+			i += 1
+
+
+		#read training file into counts
 		train_f = open(training_filename, "r")
 		for line in train_f:
 			i = 0
-			for val in line.strip().split(','):
-				self.classes[i][val] = self.classes[i][val] + 1
+			formatted_line = line.strip().split(',')
+			for val in formatted_line[:-1]:
+				classification = formatted_line[-1]
+				self.counts[i][classification][val] = self.counts[i][classification][val] + 1
 				i += 1
 		
 		train_f.close()
+	
 	
 	def classifyFile(self, infile_name, outfile_name):
 		pass
